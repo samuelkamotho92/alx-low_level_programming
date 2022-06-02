7b28bed6f294c4ce5e98594994902f543904e12d
samuelkamotho92 <samuelkamotho92@gmail.com>
2022-06-02 16:02:27 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *
  *  * main - my entry point
@@ -9,13 +8,8 @@
  *    * Return: 0
  *
  *    */
-
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
