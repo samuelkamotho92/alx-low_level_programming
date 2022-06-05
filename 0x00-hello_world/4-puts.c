1573b60e289bd407655e0909455d59e6c38470fd
samuelkamotho92 <samuelkamotho92@gmail.com>
2022-06-05 06:28:33 -0700

    commit 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- *  * main - my entry point
- *   * Description: prints "Programming is like building a multilingual puzzle"
- *    * Return: 0
- *    */
+ *entry point
+ *
+ * 
+ *return 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
