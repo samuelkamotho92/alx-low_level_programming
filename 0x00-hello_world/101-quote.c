0a5d123e9d695d5dd99680acafb6605853e02411
samuelkamotho92 <samuelkamotho92@gmail.com>
2022-06-05 07:47:19 -0700

    commit 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,10 @@ nclude <stdio.h>
 #include <unistd.h>
 
 /**
+ *main  - Entrpoint
  *
- *  * main - my entry point
- *
- *   * Description: 'and that piece of art is useful" - Dora korpar, 2015-10-19'
- *
- *    * Return: 1
- *
- *     */
+ * Return: Always 0 (success)
+ */
 
 int main(void)
 
