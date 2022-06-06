81859ac0706e10398fc1f280f1c53b72f5c81e07
samuelkamotho92 <samuelkamotho92@gmail.com>
2022-06-05 23:19:45 -0700

    changes

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 /**
- * main  - print all dgts
+ * main - Print all single digit numbers
  *
- *Return : Always 0 (success)
+ *Return: Always 0 (success)
  */
 int main(void)
 {
 	int digit;
 
-	for (digit = 0; digit < 10 ; digit++)
+		for (digit = 0; digit < 10; digit++)
 		printf("%i", digit);
 	putchar('\n');
 
