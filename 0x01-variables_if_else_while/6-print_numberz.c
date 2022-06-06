59c65aa1b899af97092dabd9e5f65e11fdb793be
samuelkamotho92 <samuelkamotho92@gmail.com>
2022-06-05 22:54:23 -0700

    commit 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main -Print digits
+ *
+ * Return:Always 0 (success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0;digit < 0; digit +++)
+		putchar(digit + '0');
+	putchar('\n');
+
+	return (0);
+
+}
