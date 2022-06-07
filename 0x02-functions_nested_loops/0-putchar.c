74549774cb3402aa557b8e9398d7a6069351d5b9
samuelkamotho92 <samuelkamotho92@gmail.com>
2022-06-07 13:24:14 -0700

    commit 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - putchar values
+ *
+ * Returnu: Always 0 (success)
+ */
+int main(void)
+{
+	char ch[8]= "_putchar";
+	int  m;
+
+	  for(m = 0; m < 8; m++)
+		   _putchar(ch);
+	        _putchar(\n);		
+  return(0);
+}
