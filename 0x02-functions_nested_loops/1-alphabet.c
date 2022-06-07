4bbe0f8b9ae99eea701c7c06cd669d63f1ae5b7d
samuelkamotho92 <samuelkamotho92@gmail.com>
2022-06-07 14:02:32 -0700

    commit 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Alwyas (0) success
+ */
+void print_alphabet(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		_putchar(alphabets);
+	}
+	_putchar('\n');
+}
+
