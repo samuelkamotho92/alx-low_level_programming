18bee761064b1a57aa035b3d39036192e1894032
samuelkamotho92 <samuelkamotho92@gmail.com>
2022-06-08 15:07:11 -0700

    commit 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,20 +1,21 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  *main - check the code
  *
  * Return: Always 0
  */
-int main(void)
+int _abs(int a)
 {
-	  int r;
-	  r = _abs(-1);
-	  printf("%d\n", r);
-	  r = _abs(0);
-	  printf("%d\n", r);
-	  r = _abs(1);
-	  printf("%d\n", r);
-          r = _abs(-98);
-          printf("%d\n", r);
-	  return (0);
+	if (a > 0)
+	{
+		a = +a;
+	}
+	else
+	{
+		a = -a;
+	}
+
+  return (0);
 }
