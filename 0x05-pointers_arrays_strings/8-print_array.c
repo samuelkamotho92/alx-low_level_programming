f724f35a179f21afdc8d16231bb2e815d13c431f
samuelkamotho92 <samuelkamotho92@gmail.com>
2022-06-14 11:26:58 -0700

    working fine

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,6 +4,8 @@
 /**
  * print_array - prints n elements of an array of integers
  * followed by a new line
+ * @a: array to be printed
+ * @n: number of elements to print
  */
 void print_array(int *a, int n)
 {
